cdb8973693d36301499b2d2db2e316023fe590ae
Ashutosh <ashutosh.na.99@gmail.com>
2024-06-24 00:10:18 +0530

    chore: use rust-overlay and update rust channel

--- flake.nix
+++ flake.nix
@@ -1,14 +1,26 @@
 {
-  description = "web nix env from determinate systems";
+    description = "web nix env from determinate systems";
 
   # Flake inputs
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/~0.2405.tar.gz";
+    rust-overlay.url = "https://github.com/oxalica/rust-overlay/archive/refs/heads/master.zip";
   };
 
   # Flake outputs
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, rust-overlay }:
     let
+      # Overlays enable you to customize the Nixpkgs attribute set
+      overlays = [
+        # Makes a `rust-bin` attribute available in Nixpkgs
+        (import rust-overlay)
+        # Provides a `rustToolchain` attribute for Nixpkgs that we can use to
+        # create a Rust environment
+        (self: super: {
+          rustToolchain = super.rust-bin.fromRustupToolchainFile ./rust-toolchain;
+        })
+      ];
+
       # Systems supported
       allSystems = [
         "x86_64-linux" # 64-bit Intel/AMD Linux
@@ -19,18 +31,18 @@
 
       # Helper to provide system-specific attributes
       forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
-        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+        pkgs = import nixpkgs { inherit overlays system; config.allowUnfree = true; };
       });
     in
     {
       # Development environment output
       devShells = forAllSystems ({ pkgs }: {
         default = pkgs.mkShell {
+          # The Nix packages provided in the environment
           packages = (with pkgs; [
-            rustc
-            rustfmt
-            cargo
-            clippy
+            # The package provided by our custom overlay. Includes cargo, Clippy, cargo-fmt,
+            # rustdoc, rustfmt, and other tools.
+            rustToolchain
             vscode
           ]) ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [ libiconv ]);
         };
